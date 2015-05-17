5a315b91e8d8117ba6c8a7ff63475edfb1d9909b
mclaughta <tom.mclaughlin@gmail.com>
2015-05-17 00:12:19 -0500

    first names, bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,9 @@
 class BootStrap {
 
     def init = { servletContext ->
+		//PART 1 - read a file off the classpath
+		def ssns = this.class.classLoader.getResourceAsStream("data/ssns.txt").text.split('\n')
+		def lastNames = this.class.classLoader.getResourceAsStream("data/last_names.txt").text.split('\n')
     }
     def destroy = {
     }
