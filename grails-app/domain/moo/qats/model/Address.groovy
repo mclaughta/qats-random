7ced978d863f00b1c2660a3f0d32ebf834630b7b
mclaughta <tom.mclaughlin@gmail.com>
2015-05-17 00:12:46 -0500

    Address, person

---
+++ grails-app/domain/moo/qats/model/Address.groovy
@@ -0,0 +1,12 @@
+package moo.qats.model
+
+class Address {
+	String line1
+	String line2
+	String city
+	String state
+	String zip
+	
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/moo/qats/model/Person.groovy
@@ -0,0 +1,10 @@
+package moo.qats.model
+
+class Person {
+	String firstName
+	String lastName
+	String ssn
+	Address address
+    static constraints = {
+    }
+}
