313854cb610613539d5c714a878908b00127046e
Tom McLaughlin <mclaughta@users.noreply.github.com>
2015-05-19 14:19:01 -0500

    Update Address.groovy

--- grails-app/domain/moo/qats/model/Address.groovy
+++ grails-app/domain/moo/qats/model/Address.groovy
@@ -10,9 +10,9 @@ class Address {
 
 	// cascade as part of Person.save(), but don't include a backreference to person	
 	static belongsTo = [Person]	
-    static constraints = {
+    	static constraints = {
 		line2 nullable:true
 		state (matches: '[A-Z]{2}')
 		zip (matches: '^[0-9]{5}(-[0-9]{4})?$')
-    }
+    	}
 }
